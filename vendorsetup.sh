3864f3d89777ad26fbaaf522766ef2e1e7fa622b
lahaina <nattolecats@gmail.com>
2024-01-27 14:12:53 +0900

    Revert "veux: [WIP] Import MIUICamera"
    This reverts commit fb9a66515c664463cbe857806218dcc6671eabbf.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,22 +1,18 @@
 echo 'Starting to clone stuffs needed for your device'
 
-echo 'Cloning Vendor tree [1/5]'
+echo 'Cloning Vendor tree [1/4]'
 # Vendor Tree
 git clone https://gitlab.com/JaswalAshish/vendor_xiaomi_veux vendor/xiaomi/veux
 
-echo 'Cloning Firmware tree [2/5]'
+echo 'Cloning Firmware tree [2/4]'
 # Firmware Tree
 git clone https://gitlab.com/JaswalAshish/vendor_xiaomi_veux-firmware.git vendor/xiaomi/veux-firmware
 
-echo 'Cloning Kernel tree [3/5]'
+echo 'Cloning Kernel tree [3/4]'
 # Kernel Tree
 git clone --depth=1 https://github.com/dereference23/kernel_xiaomi_sm6375 kernel/xiaomi/sm6375
 
-echo 'Cloning ViPER4AndroidFX [4/5]'
+echo 'Cloning ViPER4AndroidFX [4/4]'
 git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
-
-echo 'Cloning MiuiCamera [5/5]'
-git clone https://github.com/Positron-B/vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera
-git clone https://github.com/Positron-B/vendor_xiaomi_miuicamera-veux vendor/xiaomi/miuicamera-veux
 
 echo 'Completed, proceeding to lunch'
